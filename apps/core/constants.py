6f1458647b6ba4082f78c1c4524308984aa65a9f
Paul Ndambo <paulkadabo@gmail.com>
2024-07-15 20:04:27 +0300

    user model added

---
+++ apps/core/constants.py
@@ -0,0 +1,9 @@
+GENDER_CHOICES = (
+    ("Male", "Male"),
+    ("Female", "Female"),
+)
+
+USER_ROLES = (
+    ("Customer", "Customer"),
+    ("Admin", "Admin"),
+)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from apps.core.models import AbstractBaseModel
+from apps.core.constants import GENDER_CHOICES, USER_ROLES
 # Create your models here.
+class User(AbstractUser, AbstractBaseModel):
+    phone_number = models.CharField(max_length=255)
+    gender = models.CharField(max_length=50, choices=GENDER_CHOICES)
+    role = models.CharField(max_length=50, choices=USER_ROLES)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
